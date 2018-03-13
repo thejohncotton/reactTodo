285923055980b443c95eff2336d60712d70e2ae7
thejohncotton <john@1080people.com>
2018-03-13 12:30:17 -0400

    initial commit

---
+++ store.js
@@ -0,0 +1,56 @@
+var items = []
+
+var notifyComponents = function() {
+  $(ListStore).trigger('storeHasChanged')
+}
+
+var findItemById = function(id) {
+  return items.filter(function(item) {
+    return item.id === id
+  })[0]
+},
+
+ListStore = {
+
+  getItems: function() {
+    return items
+  },
+
+  loadItems: function() {
+    var loadRequest = $.ajax({
+      type: 'GET',
+      url: "http://listalous.herokuapp.com/lists/JXC-LIST"
+    })
+    loadRequest.done(function(dataFromServer) {
+  items = dataFromServer.items
+  notifyComponents()
+})
+  },
+  addItem: function(itemDescription) {
+    var creationRequest = $.ajax({
+      type: 'POST',
+      url: "http://listalous.herokuapp.com/lists/JXC-LIST/items",
+      data: { description: itemDescription, completed: false }
+    })
+    creationRequest.done(function(itemDataFromServer) {
+      items.push(itemDataFromServer)
+      notifyComponents()
+    })
+  },
+
+  toggleCompleteness: function(itemId) {
+
+    var item = findItemById(itemId)
+    var currentCompletedValue = item.completed
+    var updateRequest = $.ajax({
+      type: 'PUT',
+      url: "http://listalous.herokuapp.com/lists/JXC-LIST/items/" + itemId,
+      data: { completed: !currentCompletedValue },
+    })
+    updateRequest.done(function(itemData){
+      item.completed = itemData.completed
+      notifyComponents()
+    })
+  },
+
+}
